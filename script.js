65bed89ce0ccfe085dfdc756dfa851002c86aeae
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-14 23:36:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 fetch('https://raw.githubusercontent.com/AriAb247/Ariab247.github.io/main/Allocations_sheet.csv')
   .then(response => response.text())
-  .then(data => {
-    // Process the CSV data here
+  .then(csv => {
+    const data = Papa.parse(csv, { header: true }).data;
+    const locations = data.map(d => d.Location);
+    const specialities = data.map(d => d.Speciality);
+    console.log(locations);
+    console.log(specialities);
   });
