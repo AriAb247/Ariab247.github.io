004af0b2fa7ba3a7215b7483f1839e9b4d0faf08
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-14 23:51:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,28 @@ fetch('https://raw.githubusercontent.com/AriAb247/Ariab247.github.io/main/Alloca
   .then(response => response.text())
   .then(csv => {
     const data = Papa.parse(csv, { header: true }).data;
-    const locations = data.map(d => d.Location);
-    const specialities = data.map(d => d.Speciality);
-    console.log(locations);
-    console.log(specialities);
+
+    // Sort by Programme Title and Placement Order
+    data.sort((a, b) => {
+      if (a['Programme Title'] < b['Programme Title']) return -1;
+      if (a['Programme Title'] > b['Programme Title']) return 1;
+      return a['Placement Order'] - b['Placement Order'];
+    });
+
+    // Group data by Programme Title
+    const groupedData = {};
+    data.forEach(d => {
+      const title = d['Programme Title'];
+      if (!groupedData[title]) {
+        groupedData[title] = [];
+      }
+      groupedData[title].push({
+        location: d['Location'],
+        speciality: d['Speciality'],
+        placementOrder: d['Placement Order']
+      });
+    });
+
+    console.log(groupedData);
   });
+
