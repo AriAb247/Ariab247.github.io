f395b772efd48b8073dd2daa6cb5f89f4c16c226
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-15 00:32:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
-d3.csv('https://raw.githubusercontent.com/ariab247/ariab247.github.io/main/Allocations_sheet.csv').then(function(data) {
-  // Sort by Programme Title and Placement Order
-  data.sort((a, b) => {
-    if (a['programmeTitle'] < b['programmeTitle']) return -1;
-    if (a['programmeTitle'] > b['programmeTitle']) return 1;
-    return a['placementOrder'] - b['placementOrder'];
-  });
+d3.csv('https://raw.githubusercontent.com/ariAb247/ariab247.github.io/main/Allocations_sheet.csv')
+  .then(data => {
 
-  // Group data by Programme Title
-  const groupedData = {};
-  data.forEach(d => {
-    const title = d['programmeTitle'];
-    if (!groupedData[title]) {
-      groupedData[title] = [];
-    }
-    groupedData[title].push({
-      location: d['location'],
-      speciality: d['speciality'],
-      placementOrder: d['placementOrder']
+    // Sort by Programme Title and Placement Order
+    data.sort((a, b) => {
+      if (a['programmeTitle'] < b['programmeTitle']) return -1;
+      if (a['programmeTitle'] > b['programmeTitle']) return 1;
+      return a['placementOrder'] - b['placementOrder'];
     });
-  });
 
-  // Create table rows for each Programme Title and its allocations
-  const table = document.getElementById('allocation-table');
-  for (const title in groupedData) {
-    const allocations = groupedData[title];
-    const titleRow = document.createElement('tr');
-    const titleCell = document.createElement('td');
-    titleCell.innerText = title;
-    titleCell.setAttribute('rowspan', allocations.length);
-    titleRow.appendChild(titleCell);
-    table.appendChild(titleRow);
-    for (let i = 0; i < allocations.length; i++) {
-      const allocation = allocations[i];
-      const allocationRow = document.createElement('tr');
-      const locationCell = document.createElement('td');
-      const specialityCell = document.createElement('td');
-      locationCell.innerText = allocation.location;
-      specialityCell.innerText = allocation.speciality;
-      allocationRow.appendChild(locationCell);
-      allocationRow.appendChild(specialityCell);
-      table.appendChild(allocationRow);
+    // Group data by Programme Title
+    const groupedData = d3.group(data, d => d['programmeTitle']);
+
+    // Create table rows for each Programme Title and its allocations
+    const table = document.getElementById('allocation-table');
+    for (const [title, allocations] of groupedData) {
+      const titleRow = document.createElement('tr');
+      const titleCell = document.createElement('td');
+      titleCell.innerText = title;
+      titleCell.setAttribute('rowspan', allocations.length);
+      titleRow.appendChild(titleCell);
+      table.appendChild(titleRow);
+      for (let i = 0; i < allocations.length; i++) {
+        const allocation = allocations[i];
+        const allocationRow = document.createElement('tr');
+        const placementOrderCell = document.createElement('td');
+        const locationCell = document.createElement('td');
+        const specialityCell = document.createElement('td');
+        placementOrderCell.innerText = allocation.placementOrder;
+        locationCell.innerText = allocation.location;
+        specialityCell.innerText = allocation.speciality;
+        allocationRow.appendChild(placementOrderCell);
+        allocationRow.appendChild(locationCell);
+        allocationRow.appendChild(specialityCell);
+        table.appendChild(allocationRow);
+      }
     }
-  }
-});
-
+  });
