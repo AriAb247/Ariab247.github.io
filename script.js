5e70d836bcd80a43b286c195c7af1475de069c49
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-15 01:04:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ fetch('https://raw.githubusercontent.com/AriAb247/Ariab247.github.io/main/Alloca
   .then(response => response.text())
   .then(csv => {
     const data = Papa.parse(csv, { header: true }).data;
-    const table = document.getElementById('allocation-table');
+    const groupedData = {};
+    
+    // Group data by programmeTitle
     data.forEach(row => {
-      const tableRow = table.insertRow(-1);
-      const programmeTitleCell = tableRow.insertCell(0);
-      const locationCell = tableRow.insertCell(1);
-      const specialityCell = tableRow.insertCell(2);
-      programmeTitleCell.innerText = row.programmeTitle;
-      locationCell.innerText = row.location;
-      specialityCell.innerText = row.speciality;
+      const programmeTitle = row.programmeTitle;
+      if (!groupedData[programmeTitle]) {
+        groupedData[programmeTitle] = [];
+      }
+      groupedData[programmeTitle].push(row);
     });
+    
+    // Create table rows for each programmeTitle and its allocations
+    const table = document.getElementById('allocation-table');
+    for (const programmeTitle in groupedData) {
+      const group = groupedData[programmeTitle];
+      const row = table.insertRow(-1);
+      const programmeTitleCell = row.insertCell(0);
+      programmeTitleCell.innerText = programmeTitle;
+      group.forEach(allocation => {
+        const locationCell = row.insertCell(1);
+        const specialityCell = row.insertCell(2);
+        locationCell.innerText = allocation.location;
+        specialityCell.innerText = allocation.speciality;
+      });
+    }
   });
-
