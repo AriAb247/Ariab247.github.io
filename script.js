1c1cc39f7a2859e6b87ba6e911b61cd2bdfcb1f6
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-15 00:35:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 fetch('https://raw.githubusercontent.com/ariAb247/ariab247.github.io/main/Allocations_sheet.csv')
   .then(response => response.text())
   .then(csv => {
-    const data = d3.csvParse(csv);
+    const data = Papa.parse(csv, { header: true }).data;
 
-    // Sort by Programme Title and Placement Order
+    // Sort by programmeTitle and placementOrder
     data.sort((a, b) => {
-      if (a['Programme Title'] < b['Programme Title']) return -1;
-      if (a['Programme Title'] > b['Programme Title']) return 1;
-      return a['Placement Order'] - b['Placement Order'];
+      if (a['programmeTitle'] < b['programmeTitle']) return -1;
+      if (a['programmeTitle'] > b['programmeTitle']) return 1;
+      return a['placementOrder'] - b['placementOrder'];
     });
 
-    // Group data by Programme Title
-    const groupedData = d3.group(data, d => d['Programme Title']);
+    // Group data by programmeTitle
+    const groupedData = {};
+    data.forEach(d => {
+      const title = d['programmeTitle'];
+      if (!groupedData[title]) {
+        groupedData[title] = [];
+      }
+      groupedData[title].push({
+        location: d['location'],
+        speciality: d['speciality'],
+        placementOrder: d['placementOrder']
+      });
+    });
 
-    // Create table rows for each Programme Title and its allocations
+    // Create table rows for each programmeTitle and its allocations
     const table = document.getElementById('allocation-table');
-    groupedData.forEach((allocations, title) => {
+    for (const title in groupedData) {
+      const allocations = groupedData[title];
       const titleRow = document.createElement('tr');
       const titleCell = document.createElement('td');
       titleCell.innerText = title;
       titleCell.setAttribute('rowspan', allocations.length);
       titleRow.appendChild(titleCell);
       table.appendChild(titleRow);
-      allocations.forEach(allocation => {
+      for (let i = 0; i < allocations.length; i++) {
+        const allocation = allocations[i];
         const allocationRow = document.createElement('tr');
         const locationCell = document.createElement('td');
         const specialityCell = document.createElement('td');
-        locationCell.innerText = allocation['Location'];
-        specialityCell.innerText = allocation['Speciality'];
-        allocationRow.appendChild(document.createElement('td'));
-        allocationRow.appendChild(document.createElement('td'));
+        locationCell.innerText = allocation.location;
+        specialityCell.innerText = allocation.speciality;
         allocationRow.appendChild(locationCell);
         allocationRow.appendChild(specialityCell);
         table.appendChild(allocationRow);
-      });
-    });
-  });
+      }
     }
   });
+
