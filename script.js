f17d148aaaa395a891b704f45e10b8671aaf0ddd
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-15 00:00:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,26 @@ fetch('https://raw.githubusercontent.com/AriAb247/Ariab247.github.io/main/Alloca
       });
     });
 
-    console.log(groupedData);
+    // Create table rows for each Programme Title and its allocations
+    const table = document.getElementById('allocation-table');
+    for (const title in groupedData) {
+      const allocations = groupedData[title];
+      const titleRow = document.createElement('tr');
+      const titleCell = document.createElement('td');
+      titleCell.innerText = title;
+      titleCell.setAttribute('rowspan', allocations.length);
+      titleRow.appendChild(titleCell);
+      table.appendChild(titleRow);
+      for (let i = 0; i < allocations.length; i++) {
+        const allocation = allocations[i];
+        const allocationRow = document.createElement('tr');
+        const locationCell = document.createElement('td');
+        const specialityCell = document.createElement('td');
+        locationCell.innerText = allocation.location;
+        specialityCell.innerText = allocation.speciality;
+        allocationRow.appendChild(locationCell);
+        allocationRow.appendChild(specialityCell);
+        table.appendChild(allocationRow);
+      }
+    }
   });
-
