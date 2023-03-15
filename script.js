6d51b3ca801f87f5c7009e515096f21d51777419
AriAb247 <127565789+AriAb247@users.noreply.github.com>
2023-03-15 00:34:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-d3.csv('https://raw.githubusercontent.com/ariAb247/ariab247.github.io/main/Allocations_sheet.csv')
-  .then(data => {
+fetch('https://raw.githubusercontent.com/ariAb247/ariab247.github.io/main/Allocations_sheet.csv')
+  .then(response => response.text())
+  .then(csv => {
+    const data = d3.csvParse(csv);
 
     // Sort by Programme Title and Placement Order
     data.sort((a, b) => {
-      if (a['programmeTitle'] < b['programmeTitle']) return -1;
-      if (a['programmeTitle'] > b['programmeTitle']) return 1;
-      return a['placementOrder'] - b['placementOrder'];
+      if (a['Programme Title'] < b['Programme Title']) return -1;
+      if (a['Programme Title'] > b['Programme Title']) return 1;
+      return a['Placement Order'] - b['Placement Order'];
     });
 
     // Group data by Programme Title
-    const groupedData = d3.group(data, d => d['programmeTitle']);
+    const groupedData = d3.group(data, d => d['Programme Title']);
 
     // Create table rows for each Programme Title and its allocations
     const table = document.getElementById('allocation-table');
-    for (const [title, allocations] of groupedData) {
+    groupedData.forEach((allocations, title) => {
       const titleRow = document.createElement('tr');
       const titleCell = document.createElement('td');
       titleCell.innerText = title;
       titleCell.setAttribute('rowspan', allocations.length);
       titleRow.appendChild(titleCell);
       table.appendChild(titleRow);
-      for (let i = 0; i < allocations.length; i++) {
-        const allocation = allocations[i];
+      allocations.forEach(allocation => {
         const allocationRow = document.createElement('tr');
-        const placementOrderCell = document.createElement('td');
         const locationCell = document.createElement('td');
         const specialityCell = document.createElement('td');
-        placementOrderCell.innerText = allocation.placementOrder;
-        locationCell.innerText = allocation.location;
-        specialityCell.innerText = allocation.speciality;
-        allocationRow.appendChild(placementOrderCell);
+        locationCell.innerText = allocation['Location'];
+        specialityCell.innerText = allocation['Speciality'];
+        allocationRow.appendChild(document.createElement('td'));
+        allocationRow.appendChild(document.createElement('td'));
         allocationRow.appendChild(locationCell);
         allocationRow.appendChild(specialityCell);
         table.appendChild(allocationRow);
-      }
+      });
+    });
+  });
     }
   });
